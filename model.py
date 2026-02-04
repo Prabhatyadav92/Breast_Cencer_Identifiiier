64b035f6cb8f96cb66126597961a2bb3d159f4cd
Prabhatyadav92 <py509709@gmail.com>
2026-02-04 16:47:09 +0530

    Reformat model.py with consistent indentation

--- model.py
+++ model.py
@@ -11,5 +11,5 @@ class BreastCancerClassifier(nn.Module):
 
     def forward(self, x):
         x = self.relu(self.fc1(x))
-        x = self.fc2(x)   # NO softmax here (CrossEntropyLoss expects raw logits)
+        x = self.fc2(x)
         return x
